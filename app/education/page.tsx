6bc6381d2c3eb1458a396bec55db27bbf7e0e6af
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2026-06-19 10:48:13 +0700

    Upload app/education/page.tsx

---
+++ app/education/page.tsx
@@ -0,0 +1,9 @@
+import { EducationSection } from "@/components/Sections";
+
+export default function EducationPage() {
+  return (
+    <main className="pt-24">
+      <EducationSection />
+    </main>
+  );
+}
