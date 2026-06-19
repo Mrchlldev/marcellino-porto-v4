0198c7cad41d5d1b5f4d3c0f4f423763c070e980
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2026-06-19 10:48:11 +0700

    Upload app/contact/page.tsx

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+import { ContactSection } from "@/components/Sections";
+
+export default function ContactPage() {
+  return (
+    <main className="pt-24">
+      <ContactSection />
+    </main>
+  );
+}
