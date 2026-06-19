61b14af7cdd6f2fc8cf57f3d1da90cff8160b88c
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2026-06-19 10:48:20 +0700

    Upload app/project/page.tsx

---
+++ app/project/page.tsx
@@ -0,0 +1,10 @@
+import { CarLineupSection, ProjectSection } from "@/components/Sections";
+
+export default function ProjectPage() {
+  return (
+    <main className="pt-24">
+      <ProjectSection />
+      <CarLineupSection />
+    </main>
+  );
+}
