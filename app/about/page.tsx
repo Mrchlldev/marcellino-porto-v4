5bd6e9747d3c2e1c79bdc565817c215e9a9cf134
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2026-06-19 10:48:10 +0700

    Upload app/about/page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import { AboutSection, CarLineupSection } from "@/components/Sections";
+
+export default function AboutPage() {
+  return (
+    <main className="pt-24">
+      <AboutSection />
+      <CarLineupSection />
+    </main>
+  );
+}
