83c4763bb8ea910ae47dbfdef3cb893b407fb3ed
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2026-06-19 10:48:21 +0700

    Upload app/skill/page.tsx

---
+++ app/skill/page.tsx
@@ -0,0 +1,9 @@
+import { SkillSection } from "@/components/Sections";
+
+export default function SkillPage() {
+  return (
+    <main className="pt-24">
+      <SkillSection />
+    </main>
+  );
+}
